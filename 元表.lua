5020f4624572c259f1bedccd865e5ad27b2115c7
aigege <2221068116@qq.com>
2026-03-01 14:37:55 +0800

    元表

--- 元表.lua
+++ 元表.lua
@@ -1,35 +1,45 @@
-info = {
-    name = "aiwenjie"
-}
-obj = {
-    money = "1222"
-}
-ha = {
-    house = "BigHouse"
+-- 类对象
+Shape = {
+    area = 0
 }
 
+-- 基础类方法 new
+function Shape:new(o,side)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  side = side or 0
+  self.area = side*side;
+  return o
+end
 
-tr = {__index = obj}
-te = {__index = ha}
+-- 基础类方法 printArea
+function Shape:printArea()
+  print("面积为 ",self.area)
+end
 
-setmetatable(info, tr)
-setmetatable(obj, te)
+function Shape:printLength()
+  print("Shape长度为 ",self.area)
+end
 
-print(info.house)
+Square = Shape:new()
+-- 派生类方法 new
+function Square:new(o,side)
+  o = o or Shape:new(o,side)
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
 
-meta = {
-    ha = "haha"
-}
+-- 派生类方法 printArea
+function Square:printArea()
+  print("正方形面积为 ",self.area)
+end
 
-setmetatable(
-    meta,
-    {
-        __index = function(table, t)
-            return print(table, t)
-        end
-    }
-)
+local a = Square:new(nil,2)
+local b = Square:new(Shape,3)
+a:printArea()
+Shape:printArea()
+a:printLength()
 
-print(meta.ni)
-
-
+print(a,Square)
